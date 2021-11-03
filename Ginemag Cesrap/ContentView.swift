fe43f3bc5c96289d055808a5b95b9d45368b85de
Derek Jing <jingdian@magicdian.cn>
2021-11-03 16:06:09 +0800

    Initial Commit

---
+++ Ginemag Cesrap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ginemag Cesrap
+//
+//  Created by Derek Jing on 2021/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ginemag Cesrap/Ginemag_CesrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ginemag_CesrapApp.swift
+//  Ginemag Cesrap
+//
+//  Created by Derek Jing on 2021/11/3.
+//
+
+import SwiftUI
+
+@main
+struct Ginemag_CesrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
